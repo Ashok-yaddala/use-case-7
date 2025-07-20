76375c747386c9793b362ff32bb0fa328345744a
Ashok-yaddala <Ashokyaddala4devops@gmail.com>
2025-07-20 21:26:31 +0530

    app code changes

--- app/app.py
+++ app/app.py
@@ -1,3 +1,8 @@
-import sys
 def handler(event, context):
-    return 'Hello this is Ashok '
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/plain"
+        },
+        "body": "Hello Ashok, how are you"
+    }
